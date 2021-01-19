deb3196a94d1d3279866d605fe23828fe8a5fc29
ParkerCatz <tksm.webdesign09@gmail.com>
2021-01-19 22:24:48 +0900

    del: 不要なコードの削除

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
 import React from 'react';
-import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
+import { StyleSheet, FlatList, SafeAreaView } from 'react-native';
 import ListItem from './components/ListItem';
 import articles from './dummies/articles';
 
 export default function App() {
-  const items = articles.map((article, index) => {
-    return (
-      <ListItem
-        imageUrl={article.urlToImage}
-        title={article.title}
-        author={article.author}
-        author={article.author}
-      />
-    );
-  });
-
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
