eab8ba526f89dd1cd72b5cb5f01e026960cc7cee
ParkerCatz <tksm.webdesign09@gmail.com>
2021-01-22 01:40:15 +0900

    add: redux-persistで状態をローカルストレージに保存

--- store/index.js
+++ store/index.js
@@ -1,12 +1,23 @@
 import { createStore, combineReducers } from 'redux';
 import userReducers from './reducers/user';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistReducer, persistStore } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
 
 // ツリー構造にしていく
 const rootReducer = combineReducers({
   user: userReducers,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['user'], // 必要なデータのみ保存する(ツリー)（ホワイトリスト形式、ブラックリスト形式）
+};
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, composeWithDevTools());
+
+export const persistor = persistStore(store);
 export default store;
