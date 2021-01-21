325881269af8e12dfa080dbb00d642b4f69f1f0e
ParkerCatz <tksm.webdesign09@gmail.com>
2021-01-21 23:19:08 +0900

    add: デバッガーのインストール

--- store/index.js
+++ store/index.js
@@ -1,11 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import userReducers from './reducers/user';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 // ツリー構造にしていく
 const rootReducer = combineReducers({
   user: userReducers,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
