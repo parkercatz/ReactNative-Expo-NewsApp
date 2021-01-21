48152fb71793ff240791f6df92099cb3e7f28546
ParkerCatz <tksm.webdesign09@gmail.com>
2021-01-21 23:38:02 +0900

    improve: ActionのDispatchの実装

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, TouchableOpacity } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { useDispatch } from 'react-redux';
+import { addClip, deleteClip } from '../store/actions/user';
 
 export default ArticleScreenshots = ({ route }) => {
   const { article } = route.params;
-  console.log(article);
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(addClip({ clip: article }));
+        }}
+      >
+        <Text style={{ margin: 10, fontSize: 30 }}>ADD_CLIP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(deleteClip({ clip: article }));
+        }}
+      >
+        <Text style={{ margin: 10, fontSize: 30 }}>DELETE_CLIP</Text>
+      </TouchableOpacity>
       <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   );
